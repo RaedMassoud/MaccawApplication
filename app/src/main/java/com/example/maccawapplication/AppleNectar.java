187ca5a560857c299ed7af265debefbbb272434b
Raed Massoud <massoud.r@outlook.com>
2024-02-28 22:47:17 +0200

    Initial Commit

---
+++ app/src/main/java/com/example/maccawapplication/AppleNectar.java
@@ -0,0 +1,46 @@
+package com.example.maccawapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.Spinner;
+
+import com.google.android.material.snackbar.Snackbar;
+
+public class AppleNectar extends AppCompatActivity {
+
+    Button buttonOrder;
+    Spinner spinner;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_apple_nectar);
+
+        //      Connect spinner by id
+        spinner = findViewById(R.id.spinnerPurchaseQuantity);
+
+
+//      Order Button setup
+        buttonOrder = findViewById(R.id.buttonOrder);
+        buttonOrder.setOnClickListener( (view) -> {
+
+            double pricePerUnit = 26.013; // price per unit
+
+//          Get and store selected quantity
+            int quantity = Integer.parseInt(spinner.getSelectedItem().toString());
+
+//          Compute total
+            double total = (double) quantity * pricePerUnit;
+
+//          Create message
+            String message = String.format("%d Apple Nectar ordered! Total is LBP %.3f", quantity, total);
+
+//          Make and show snake bar
+            Snackbar.make(buttonOrder, message, Snackbar.LENGTH_SHORT).show();
+
+        });
+
+    }
+}
